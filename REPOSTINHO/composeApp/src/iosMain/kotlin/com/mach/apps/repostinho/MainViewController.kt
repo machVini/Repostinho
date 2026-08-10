e61db5653728c9b9643dae0fc25055439c5e94c5
Vinicius Machado <viniciusmach07@gmai.com>
2026-08-10 11:40:18 -0300

    add ios MainViewController and app icon

--- REPOSTINHO/composeApp/src/iosMain/kotlin/com/mach/apps/repostinho/MainViewController.kt
+++ REPOSTINHO/composeApp/src/iosMain/kotlin/com/mach/apps/repostinho/MainViewController.kt
@@ -2,15 +2,23 @@ package com.mach.apps.repostinho
 
 import androidx.compose.ui.window.ComposeUIViewController
 import com.mach.apps.repostinho.di.initKoin
+import platform.UIKit.UIViewController
 
-fun MainViewController() = ComposeUIViewController {
-    initKoinOnce()
-    App()
+/**
+ * Entrada do app no iOS, chamada pelo `ComposeView` do SwiftUI.
+ *
+ * O Koin sobe aqui, fora da composição: no Android isso acontece na `Application`, e o
+ * iOS não tem equivalente. O guarda existe porque o SwiftUI pode recriar o
+ * `UIViewControllerRepresentable`, e `startKoin` duas vezes lança exceção.
+ */
+fun MainViewController(): UIViewController {
+    startKoinOnce()
+    return ComposeUIViewController { App() }
 }
 
 private var koinStarted = false
 
-private fun initKoinOnce() {
+private fun startKoinOnce() {
     if (!koinStarted) {
         koinStarted = true
         initKoin()
